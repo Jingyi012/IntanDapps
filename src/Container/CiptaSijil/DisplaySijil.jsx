a30d2d2a3e7d4d5d02374fea25ba074f62954f52
niczrsoh <87169349+niczrsoh@users.noreply.github.com>
2023-04-27 23:42:36 +0800

    integrate cipta sijil with frontend

---
+++ src/Container/CiptaSijil/DisplaySijil.jsx
@@ -0,0 +1,59 @@
+import React, { useState,useContext,useEffect} from 'react';
+import algosdk from 'algosdk';
+import {useNavigate, useParams } from "react-router-dom";
+import AppContext ,{ AppContextProvider } from '../../Context/AppContext';
+import { indexerClient } from '../../Constant/ALGOkey';
+const DisplaySijil = () =>{
+
+    const transId = useParams();
+    const [tajuk,setTajukSijil] = useState('');
+    const [mula,setTarikhMula] = useState('');
+    const [tamat,setTarikhTamat] = useState('');
+    const [nama,setNama] = useState('');
+    const [courseAction, setCourseAction] = useState('');
+    const { account, setAccount } = useContext(AppContext);
+    const algodClient = new algosdk.Algodv2('QZ584lQ7DF7U2somENFzV7GwLGNbiNdB82oPh7j3', 'https://node.testnet.algoexplorerapi.io', '');
+    console.log(transId.transId);
+
+    const getAppArgs = async()=>{
+
+      console.log("hello");
+      console.log(transId.appId);
+      const transInfo = await indexerClient.lookupTransactionByID(transId.transId).do();
+      console.log(transInfo.transaction["application-transaction"]["application-args"][0]);
+      const decoder = new TextDecoder();
+      const dTajuk = window.atob(transInfo.transaction["application-transaction"]["application-args"][0]);
+      const dMula = window.atob(transInfo.transaction["application-transaction"]["application-args"][1]);
+      const dTamat = window.atob(transInfo.transaction["application-transaction"]["application-args"][2]);
+      const dNama = window.atob(transInfo.transaction["application-transaction"]["application-args"][3]);
+      const onComplete = transInfo.transaction["application-transaction"]["on-completion"];
+      const tajuk = Object.values(JSON.parse(dTajuk))[0];
+      const mula = Object.values(JSON.parse(dMula))[0];
+      const tamat = Object.values(JSON.parse(dTamat))[0];
+      const nama = Object.values(JSON.parse(dNama))[0];
+      setTajukSijil(tajuk);
+      setTarikhMula(mula);
+      setTarikhTamat(tamat);
+      setNama(nama);
+      setCourseAction(onComplete);
+    }
+
+    return (<div>
+    <h1>Confirmation Page</h1>
+    <div>
+      <button onClick={async()=>{
+        await getAppArgs();
+      }}>View information
+      </button>
+      <br></br>
+      <label htmlFor="courseInfo">Sijil Information:</label>
+      <br></br>
+      <p>Tajuk Sijil: {tajuk.replaceAll('"','')}</p>
+      <p>Tarikh Mula: {mula.replaceAll('"','')}</p>
+      <p>Tarikh Tamat: {tamat.replaceAll('"','')}</p>
+      <p>Nama: {nama.replaceAll('"','')}</p>
+      <p>Course Action: {courseAction.replaceAll('"','')}</p>
+    </div>
+  </div>);
+}
+export default DisplaySijil;
